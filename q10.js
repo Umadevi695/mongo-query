b0aa8fe153abaab9892723ca4bd9bead90cc6a81
Umadevi695 <uma970179@gmail.com>
2025-05-30 19:28:54 +0530

    up

--- q10.js
+++ q10.js
@@ -1,31 +1,26 @@
-print("Top 3 Highest Paid Employees:");
-db.employees
-  .aggregate([{ $sort: { monthlySalary: -1 } }, { $limit: 3 }])
-  .forEach((doc) => printjson(doc));
+// 1. Display Top 3 Highest Paid Employees
+db.employees.aggregate([{ $sort: { monthlySalary: -1 } }, { $limit: 3 }]);
 
-print("\nAnnual Salary of Each Employee:");
-db.employees
-  .aggregate([
-    {
-      $project: {
-        name: 1,
-        annualSalary: { $multiply: ["$monthlySalary", 12] },
+// 2. Display Annual Salary of Each Employee
+db.employees.aggregate([
+  {
+    $project: {
+      name: 1,
+      monthlySalary: 1,
+      annualSalary: { $multiply: ["$monthlySalary", 12] },
+    },
+  },
+]);
+
+// 3. Display Band Based on Age (Band A if age > 40, else Band B)
+db.employees.aggregate([
+  {
+    $project: {
+      name: 1,
+      age: 1,
+      band: {
+        $cond: { if: { $gt: ["$age", 40] }, then: "Band A", else: "Band B" },
       },
     },
-  ])
-  .forEach((doc) => printjson(doc));
-
-print("\nBand Based on Age:");
-db.employees
-  .aggregate([
-    {
-      $project: {
-        name: 1,
-        age: 1,
-        band: {
-          $cond: { if: { $gt: ["$age", 40] }, then: "Band A", else: "Band B" },
-        },
-      },
-    },
-  ])
-  .forEach((doc) => printjson(doc));
+  },
+]);
